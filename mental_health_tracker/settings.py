dc4102e13dc5f43e9be8fc9c8007616638307336
andiaqsa <andiaqsa365@gmail.com>
2024-08-28 23:34:42 +0700

    aqsaa

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f)ay328-+8j-_&o^+--x8eqi(g_e!5)6pmvwra30p@lg^==89!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","andi-aqsa--mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
